6b576e02a9402fae41521965346000a92f6b7254
Areg Harutyunyan <harutyunyanareg2011@gmail.com>
2022-10-29 19:06:15 -0600

    Make compile-ts compile both files

--- compile-ts.bat
+++ compile-ts.bat
@@ -1,6 +1,7 @@
 @echo off
 
 echo Compiling main.ts
-npx tsc .\ts\main.ts --outDir ./js
+call npx tsc .\ts\main.ts --outDir ./js
+
 echo Compiling 404.ts
-npx tsc .\ts\404.ts --outDir ./js
+call npx tsc .\ts\404.ts --outDir ./js
