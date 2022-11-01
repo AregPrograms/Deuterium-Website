800ce494593868c8a67161c932069f77c1d9a482
Areg Harutyunyan <harutyunyanareg2011@gmail.com>
2022-11-01 17:21:13 -0600

    Make .gitignore work

--- compile-ts.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Compiling main.ts
-call npx tsc .\ts\main.ts --outDir ./js
-
-echo Compiling 404.ts
-call npx tsc .\ts\404.ts --outDir ./js
--- terminal/scripts/compile.bat
+++
@@ -1,1 +0,0 @@
-call npx tsc terminal.ts --outDir ./js
